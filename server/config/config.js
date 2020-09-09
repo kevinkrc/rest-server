700dcbf08cdd3d2fbb45eb4debae33130b135d41
kevinkrc <k_cantera21@hotmail.com>
2020-09-09 14:53:49 -0500

    Npm listo

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,32 @@
+// ============================
+// Puerto
+
+const { mongo } = require("mongoose");
+
+// ============================
 process.env.PORT = process.env.PORT || 3000;
+
+// ============================
+// Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV ||'dev';
+
+// ============================
+// Base de Datos
+// ============================
+let urlDB;
+
+// if (process.env.NODE_ENV === 'dev'){
+//     urlDB = 'mongodb://localhost:27017/cafe';
+// }else{
+    urlDB = 'mongodb+srv://necvin:E3VXtW49L0j57ihe@cluster0.goaq7.mongodb.net/cafe?retryWrites=true&w=majority';
+// }
+
+process.env.URLDB = urlDB;
+
+
+
+
+
+
+
--- server/server.js
+++ server/server.js
@@ -2,50 +2,30 @@ require('./config/config')
 
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.use(require('./routes/routes'));
  
-app.get('/usuario', function (req, res) {
-     res.json('get Usuarios')
-})
 
-app.post('/usuario', function (req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-})
-
-app.put('/usuario/:id', function (req, res) {
-
-    let id = req.params.id
-
-    res.json({
-        id
-    })
-})
-
-app.delete('/usuario', function (req, res) {
-    res.json('get Usuarios')
-})
    
+mongoose.connect(process.env.URLDB, 
+{useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology:true }, 
+
+(err, res) => {
+    if(err) throw err;
+
+    console.log('Base de datos online');
+});
+
+
 app.listen(process.env.PORT, () => {
     console.log("Escuchando puerto: ", process.env.PORT);
 });
